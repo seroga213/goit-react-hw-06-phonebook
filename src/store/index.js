1ab129a5869cf982c6dc9cc8c7ffd25e4eeb979f
Seroga Cherkassov <serogaseroga05@gmail.com>
2022-11-20 14:38:22 +0200

    .

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
-import contactsSlice from './reducer';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { 
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({
-  reducer: {
-    contactsSlice,
-  },
+import todoReducer from './reducer';
+
+const rootReducer = combineReducers({
+  todos: todoReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store);
+export default store;
